33b81423ec2470f6475f1e26c09cac3909e50a63
aleksANJJ <meshkova8171@gmail.com>
2022-01-31 12:58:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
+
+  const [dog, setDog] = useState(0);
+
+  const {nameDog, image, description, care, price} = data[dog];
+  
+const nextDog =() => {
+  setDog((dog => {
+    dog++;
+  if (dog > data.length-1) {
+    dog=0;
+  }
+  return dog;
+}))
+}
+const previosDog =()=> {
+  setDog((dog => {
+    dog--;
+    if(dog < 0) {
+      return data.length-1;
+    }
+    return dog;
+  }))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <div className="container">
+       <h1>Какую собаку выберишь ты?</h1>
+     </div>
+     <div className="container">
+       <img src={image} width="450px" alt="fotoDog"/>
+     </div>
+     <div className="container">
+       <h2>{nameDog}</h2>
+     </div>
+     <div className="container">
+       <p>{description}</p>
+     </div>
+     <div className="container">
+       <p>{care}</p>
+     </div>
+     <div className="container">
+       <h3>Цена щенка от {price}</h3>
+     </div>
+     <div className="btn">
+       <button className="previos" onClick={previosDog}>PREVIOS</button>
+       <button className="next" onClick={nextDog}>NEXT</button>
+     </div>
+   </div>
   );
 }
 
